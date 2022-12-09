7baa69194df263cbec627c5ff9ff24f6081a0840
Marija <marija@dailyart.eu>
2022-12-09 10:11:57 +0200

    fix missing score

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,8 +7,8 @@ import styles from './header.module.scss';
 
 function Header() {
   const [QuizState] = useContext(QuizContext);
-  const { score } = QuizState.score;
-
+  const { score } = QuizState;
+ 
   return (
     <div className={styles.Header_Container}>
       <div>
