a89fd72d71793cb59a238f26dbe75ea8597ea54e
Marija <marija@dailyart.eu>
2022-12-16 13:50:53 +0200

    formik login

--- src/pages/loginform/loginform.component.jsx
+++ src/pages/loginform/loginform.component.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { useFormik } from 'formik';
@@ -6,10 +7,10 @@ import { useFormik } from 'formik';
 import styles from './loginform.module.scss';
 
 function LoginForm() {
-
   const formik = useFormik({
     initialValues: {
       email: '',
+      password: '',
     },
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
@@ -17,19 +18,35 @@ function LoginForm() {
   });
   return (
     <>
-    <Link to='/signup'>SignUp</Link>
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
+      <Link to="/login/">SignUp</Link>
+      <Link to="/signup/">SignUp</Link>
+      <form onSubmit={formik.handleSubmit} className={styles.Group}>
+        <label htmlFor="email" className={styles.Form_InputLabel}>
+          Email Address
+        </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          className={styles.Form_Input}
+        />
 
-      <button type="submit">Submit</button>
-    </form>
+        <label htmlFor="password" className={styles.Form_InputLabel}>
+          Email Address
+        </label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          className={styles.Form_Input}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
