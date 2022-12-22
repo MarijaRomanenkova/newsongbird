68c81c7d3e18ecd011bfa4353ca29598a1701e7e
Marija <marija@dailyart.eu>
2022-12-22 17:52:20 +0200

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, Outlet } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Home from 'pages/home/home';
 import Navigation from 'components/navigation/navigation.component';
