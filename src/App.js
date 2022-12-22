59197413b0c91864db905d08477bb13998cc8792
Marija <marija@dailyart.eu>
2022-12-22 16:45:58 +0200

    change

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import Navigation from 'components/navigation/navigation.component';
 import LoginForm from 'pages/loginform/loginform.component';
 import SignUp from 'pages/signup/signup';
 import NotFound from 'pages/notfound/notfound.component';
+import availableRoutesList from './routes/availableRoutesList';
 
 function App() {
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path={availableRoutesList.HOME} element={<Home />} />
+        <Route path={availableRoutesList.LOGIN} element={<LoginForm />} />
+        <Route path={availableRoutesList.SIGN_UP} element={<SignUp />} />
+        <Route path={availableRoutesList.NOT_FOUND} element={<NotFound />} />
       </Routes>
     </>
   );
--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 import logoSourceSVG from 'assets/logoSourceSVG.svg';
 import Score from 'components/score/score.component';
-
+import availableRoutesList from 'routes/availableRoutesList';
 import styles from './navigation.module.scss';
 
 function Navigation() {
   return (
     <nav className={styles.Navigation_Container}>
       <NavLink to="/">
+        <Outlet />
         <div>
           <img className={styles.Logo} src={logoSourceSVG} alt="Logo" />
         </div>
       </NavLink>
       <div className={styles.Navigation_Link_Wrapper}>
-        <NavLink className={styles.Navigation_Link} to="/login">
+        <NavLink
+          className={styles.Navigation_Link}
+          to={availableRoutesList.LOGIN}
+        >
           Login
         </NavLink>
-        <NavLink className={styles.Navigation_Link} to="/signup">
+        <NavLink
+          className={styles.Navigation_Link}
+          to={availableRoutesList.SIGN_UP}
+        >
           Sign Up
         </NavLink>
       </div>
