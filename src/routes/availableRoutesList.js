06ef661fa752c1157adcdcf765fe8a7d8e1ddac8
Marija <marija@dailyart.eu>
2022-12-22 18:30:50 +0200

    IT WORKS

--- src/routes/availableRoutesList.js
+++ src/routes/availableRoutesList.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const availableRoutesList = {
-  HOME: '/home',
+  HOME: '/',
   LOGIN: '/login',
-  SIGN_UP: 'sign-up',
-  NOT_FOUND: 'notfound',
+  SIGN_UP: '/sign-up',
+  NOT_FOUND: '/notfound',
 };
