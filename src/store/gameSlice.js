6f1f7ec507e0fc1b252ba3f4f678c110eaecd6ea
Marija <marija@dailyart.eu>
2022-12-23 16:45:06 +0200

    deleted smth

--- src/store/gameSlice.js
+++ src/store/gameSlice.js
@@ -22,21 +22,6 @@ const initialState = {
   isGameOver: false,
 };
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
-
-// export const incrementAsync = createAsyncThunk(
-//   'counter/fetchCount',
-//   async (amount) => {
-//     const response = await fetchCount(amount);
-//     // The value we return becomes the `fulfilled` action payload
-//     return response.data;
-//   }
-// );
-
 export const gameSlice = createSlice({
   name: 'game',
   initialState,
@@ -103,14 +88,5 @@ export const selectCurrentCorrectAnswerObject = (state) =>
   state.game.birdsData[state.game.currentLevel][state.game.correctAnswerID - 1];
 export const selectCurrentCategoryArray = (state) =>
   state.game.birdsData[state.game.currentLevel];
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-// `export const incrementIfOdd = (amount) => (dispatch, getState) => {
-//   const currentValue = selectCount(getState());
-//   if (currentValue % 2 === 1) {
-//     dispatch(incrementByAmount(amount));
-//   }
-// };`
 
 export default gameSlice.reducer;
