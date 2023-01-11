e9a5de987e3e2c830935a2cabb3b5ac1a2494578
Marija <marija@dailyart.eu>
2023-01-11 09:25:04 +0200

    nav styling

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -7,7 +7,6 @@ import Score from 'components/score/score.component';
 import { availableRoutesList } from 'routes/availableRoutesList';
 import LanguageSwitch from 'components/language-switch/index';
 
-
 import styles from './navigation.module.scss';
 
 function Navigation() {
@@ -18,22 +17,30 @@ function Navigation() {
           <img className={styles.Logo} src={logoSourceSVG} alt="Logo" />
         </div>
       </NavLink>
-      <div className={styles.Navigation_Link_Wrapper}>
-        <NavLink
-          className={styles.Navigation_Link}
-          to={availableRoutesList.LOGIN}
-        >
-          <FormattedMessage id="login" defaultMessage="Login" />
-        </NavLink>
-        <NavLink
-          className={styles.Navigation_Link}
-          to={availableRoutesList.SIGN_UP}
-        >
-          <FormattedMessage id="signup" defaultMessage="Sign Up" />
-        </NavLink>
+
+      <div className={styles.Navigation_Score_Wrapper}>
+        <Score />
       </div>
-      <LanguageSwitch />
-      <Score />
+
+      <div className={styles.Navigation_Link_Wrapper}>
+        <div className={styles.Navigation_Link_InnerWrapper}>
+          <NavLink
+            className={styles.Navigation_Link}
+            to={availableRoutesList.LOGIN}
+          >
+            <FormattedMessage id="login" defaultMessage="Login" />
+          </NavLink>
+          <NavLink
+            className={styles.Navigation_Link}
+            to={availableRoutesList.SIGN_UP}
+          >
+            <FormattedMessage id="signup" defaultMessage="Sign Up" />
+          </NavLink>
+        </div>
+        <div className={styles.Navigation_LanguageSwitch_Wrapper}>
+          <LanguageSwitch />
+        </div>
+      </div>
     </nav>
   );
 }
