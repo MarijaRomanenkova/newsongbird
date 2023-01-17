fd6c82c18d287ad71bb00c4f93d197d77c86f11e
Marija <marija@dailyart.eu>
2023-01-17 16:13:43 +0200

    cleanup

--- src/lang/languageList.js
+++ src/lang/languageList.js
@@ -5,24 +5,18 @@ import flagGBsourceSVG from 'assets/flagGBsourceSVG.svg';
 
 export const languageList = [
   {
-    locale: 'en-EN',
     name: 'English',
     code: 'en',
     img: flagGBsourceSVG,
-    url: '/dataen.json',
   },
   {
-    locale: 'ru-RU',
     name: 'ru',
     code: 'ru',
     img: flagRUsourceSVG,
-    url: '/dataru.json',
   },
   {
-    locale: 'lt-LT',
     name: 'Lithuanian',
     code: 'lt',
     img: flagLTsourceSVG,
-    url: '/datalt.json',
   },
 ];
