ba487d584aae53094983f1aab034d3f155111b03
Marija <marija@dailyart.eu>
2023-01-18 15:55:42 +0200

    another json

--- src/components/categories/categories.component.jsx
+++ src/components/categories/categories.component.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+// import { useTranslation } from 'react-i18next';
 
 import { selectCurrentLevel, selectCategoriesNames } from 'store/gameSlice';
 
 import styles from 'components/categories/categories.module.scss';
 
 function Categories() {
+  // const { i18n } = useTranslation();
+  // eslint-disable-next-line prefer-destructuring
+  // const language = i18n.language;
   const currentLevel = useSelector(selectCurrentLevel);
   const categoriesNames = useSelector(selectCategoriesNames);
+  const neededObject = categoriesNames[];
+  console.log(categoriesNames);
+  console.log(neededObject)
+ 
 
   return (
     <div className={styles.Categories__Container}>
       {categoriesNames.map((category, index) => (
         <div
-          key={category.uniqueID}
+          key={category}
           className={
             index === currentLevel - 1
               ? styles.Category_Active
