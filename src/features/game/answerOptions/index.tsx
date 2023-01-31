972629518dc98007b105427d6fb9c83cd1985dfb
Marija <marija@dailyart.eu>
2023-01-31 13:22:33 +0200

    fixes

--- src/features/game/answerOptions/index.tsx
+++ src/features/game/answerOptions/index.tsx
@@ -1,70 +1,161 @@
-import React, { useRef } from 'react';
-import H5AudioPlayer from 'react-h5-audio-player';
-import { useAppSelector } from 'app/hooks';
+/* eslint-disable no-inner-declarations */
+import React, { useState, useEffect } from 'react';
+import { useSound } from 'use-sound';
+import { useAppSelector, useAppDispatch } from 'app/hooks';
+import { nanoid } from 'nanoid';
+import { useTranslation } from 'react-i18next';
 
-import { selectIsCorrectAnswerChosen } from 'features/game/gameSlice';
+import correctAnswerChosenSoundOGG from 'shared/assets/sounds/correctAnswerChosenSound.ogg';
+import incorrectAnswerChosenSoundOGG from 'shared/assets/sounds/incorrectAnswerChosenSound.ogg';
+import AnswerOptionDetails from 'features/game/answerOptionDetails/index';
+import Circle from 'shared/ui/circle/index';
+import Button from 'shared/ui/button/index';
+import {
+  selectCorrectAnswerID,
+  selectIsGameOver,
+  switchToNextLevel,
+  correctAnswerChosen,
+  answerWasChosen,
+  selectBirdsData,
+  selectCurrentLevel,
+} from 'features/game/gameSlice';
+import { Option, CurrentAnswerOptionsArray } from 'shared/interfaces';
 
 import styles from './index.module.scss';
 
-type Props = {
-  image: string;
-  name: any;
-  species: string;
-  description: string;
-  audio: string;
-};
+function AnswerOptions() {
+  const dispatch = useAppDispatch();
+  const { t, i18n } = useTranslation();
 
-const AnswerOptionDetails = ({
-  image,
-  name,
-  description,
-  species,
-  audio,
-}: Props): JSX.Element => {
-  const isCorrectAnswerChosen: boolean = useAppSelector(
-    selectIsCorrectAnswerChosen
+  const currentLevel = useAppSelector(selectCurrentLevel);
+  const birdsData = useAppSelector(selectBirdsData);
+  const correctAnswerID = useAppSelector(selectCorrectAnswerID);
+  const isGameOver = useAppSelector(selectIsGameOver);
+
+  const [playCorrectAnswerChosenSound] = useSound(correctAnswerChosenSoundOGG);
+  const [playIncorrectAnswerChosenSound] = useSound(
+    incorrectAnswerChosenSoundOGG
   );
+  const [isNextButtonDisabled, setIsNextButtonDisabled] =
+    useState<boolean>(true);
+  const [currentCategoryOptions, setCurrentCategoryOptions] =
+    useState<CurrentAnswerOptionsArray>([]);
+  const [currentChosenAnswer, setCurrentChosenAnswer] = useState<Option>();
 
-  const AudioPlayerREF: any = useRef<H5AudioPlayer>(null);
-  const pauseAudioPlayer = () => {
-    if (AudioPlayerREF !== null) {
-      AudioPlayerREF.current.audio.current.pause();
-    }
-  };
-
-  if (isCorrectAnswerChosen) {
-    pauseAudioPlayer();
+  const { language } = i18n;
+  const currentCategoryOptionsByLanguage = birdsData[language];
+  function findCurrentLevelByIndex(option: Option, index: number): boolean {
+    return index === currentLevel;
   }
 
+  useEffect(() => {
+    if (currentLevel && currentCategoryOptionsByLanguage) {
+      setCurrentCategoryOptions(
+        currentCategoryOptionsByLanguage
+          .find((option, index) => findCurrentLevelByIndex(option, index))
+          .map((option) => {
+            if (option.id === correctAnswerID) {
+              return {
+                ...option,
+                uniqueID: nanoid(),
+                isTouched: false,
+                isCorrectAnswer: true,
+              };
+            }
+            return {
+              ...option,
+              uniqueID: nanoid(),
+              isTouched: false,
+              isCorrectAnswer: false,
+            };
+          })
+      );
+    }
+  }, [currentLevel, currentCategoryOptionsByLanguage]);
+
+  function handleAnswerOptionClick(id) {
+    setCurrentChosenAnswer(
+      currentCategoryOptions.find((option) => option.id === id)
+    );
+    if (!isNextButtonDisabled) {
+      return;
+    }
+    dispatch(answerWasChosen(id));
+    setCurrentCategoryOptions(
+      currentCategoryOptions.map((option) => {
+        if (option.id === id) {
+          return { ...option, isTouched: true };
+        }
+        return { ...option };
+      })
+    );
+
+    if (id === correctAnswerID) {
+      dispatch(correctAnswerChosen());
+      playCorrectAnswerChosenSound();
+      if (!isGameOver) {
+        setIsNextButtonDisabled(false);
+      }
+    }
+
+    playIncorrectAnswerChosenSound();
+  }
+
+  const handleNextButtonClick = () => {
+    setIsNextButtonDisabled(true);
+    dispatch(switchToNextLevel());
+  };
+
   return (
-    <div className={styles.AnswerOptionDetails_Container}>
-      <img
-        className={styles.AnswerOptionDetails_Image}
-        src={image}
-        alt={name}
-      />
-      <div>
-        <h2 className={styles.AnswerOptionDetails_Name_Text}>{name}</h2>
-        <h4 className={styles.AnswerOptionDetails_Species_Text}>{species}</h4>
-        {audio && (
-          <H5AudioPlayer
-            layout="horizontal-reverse"
-            src={audio}
-            autoPlay={false}
-            autoPlayAfterSrcChange={false}
-            showJumpControls={false}
-            showFilledProgress
-            customAdditionalControls={[]}
-            customVolumeControls={[]}
-            ref={AudioPlayerREF}
+    <>
+      <div className={styles.AnswerOptions_Container}>
+        <div className={styles.AnswerOptionsList_Container}>
+          {currentCategoryOptions.map((option) => (
+            <button
+              key={option.uniqueID}
+              className={styles.AnswerOptionsList_Option}
+              type="button"
+              onClick={() => handleAnswerOptionClick(option.id)}
+              disabled={!isNextButtonDisabled}
+            >
+              <Circle
+                isTouched={option.isTouched}
+                isCorrectAnswer={option.isCorrectAnswer}
+              />
+              {option.name}
+            </button>
+          ))}
+        </div>
+
+        {currentChosenAnswer.id && (
+          <AnswerOptionDetails
+            currentChosenAnswerName={currentChosenAnswer.name}
+            currentChosenAnswerImage={currentChosenAnswer.image}
+            currentChosenAnswerDescription={currentChosenAnswer.description}
+            currentChosenAnswerAudio={currentChosenAnswer.audio}
+            species={currentChosenAnswer.species}
           />
         )}
+        {!currentChosenAnswer.id && (
+          <div className={styles.AnswerOptionDetails_Dummy}>
+            <h4 className={styles.AnswerOptionDetails_Dummy_Text}>
+              Послушайте плеер.
+            </h4>
+            <h4 className={styles.AnswerOptionDetails_Dummy_Text}>
+              Выберите птицу из списка
+            </h4>
+          </div>
+        )}
       </div>
-      <div className={styles.AnswerOptionDetails_Description}>
-        {description}
-      </div>
-    </div>
+      <Button
+        isHidden={isGameOver}
+        isDisabled={isNextButtonDisabled}
+        handleClick={handleNextButtonClick}
+        name={t('next-level')}
+        type="button"
+      />
+    </>
   );
-};
+}
 
-export default AnswerOptionDetails;
+export default AnswerOptions;
