87c14bb496e73f6f9035df04b85d298b81de44b9
Marija <marija@dailyart.eu>
2023-03-15 15:30:20 +0200

    fixes

--- src/shared/ui/language-switch/index.tsx
+++ src/shared/ui/language-switch/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 
 import { languageList } from 'shared/languageList';
+import { switchLanguage } from 'features/game/gameSlice';
 
 import styles from './index.module.scss';
 
 function LanguageSwitch() {
   const { i18n } = useTranslation();
+  const dispatch = useDispatch();
+
+  function handleSwitch(code: string) {
+    i18n.changeLanguage(code);
+    dispatch(switchLanguage(code));
+  }
 
   return (
     <div className={styles.LanguageSwitch_Container}>
@@ -14,7 +22,7 @@ function LanguageSwitch() {
         <button
           key={code}
           value={code}
-          onClick={() => i18n.changeLanguage(code)}
+          onClick={() => handleSwitch(code)}
           type="button"
           className={styles.LanguageSwitch_Flag}
         >
